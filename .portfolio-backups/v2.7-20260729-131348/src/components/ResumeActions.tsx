ee69f02fa71059022ecbf363c488e3059bf99afb
Jaswant Sai Krishna Donadi <jaswant.saikrishna@gmail.com>
2026-07-29 19:47:48 +0530

    Remove old portfolio backup versions

--- .portfolio-backups/v2.7-20260729-131348/src/components/ResumeActions.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function ResumeActions() {
-  return (
-    <button className="pill-button resume-print" type="button" onClick={() => window.print()}>
-      Print / Save as PDF
-    </button>
-  );
-}
--- .portfolio-backups/v2.9-20260729-190714/src/app/resume/print/page.tsx
+++
@@ -1,7 +0,0 @@
-import PrintResumePage from "@/components/PrintResumePage";
-
-export const metadata = { title: "Print Resume" };
-
-export default function ResumePrintRoute() {
-  return <PrintResumePage />;
-}
